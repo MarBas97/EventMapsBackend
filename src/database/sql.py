b04ece0ef35d6efa1f6623ad7e002ba8f3b5850f
Mario Bas <mariobas@wp.pl>
2021-01-06 16:09:36 +0100

    Added description and likes columns to pointers table

--- src/database/sql.py
+++ src/database/sql.py
@@ -6,7 +6,7 @@ DATABASE = 'database.db'
 conn = sqlite3.connect(DATABASE)
 # Stworzenie tabeli w bazie danych za pomocą sqlite3
 conn.execute('CREATE TABLE IF NOT EXISTS users (userId Integer PRIMARY KEY, email TEXT UNIQUE NOT NULL, username TEXT UNIQUE NOT NULL, password TEXT, isAdmin BOOLEAN)')
-conn.execute('CREATE TABLE IF NOT EXISTS mapPointers (pointerId Integer PRIMARY KEY, long REAL NOT NULL, lan REAL NOT NULL, createdOn TEXT NOT NULL, userId INTEGER NOT NULL, FOREIGN KEY(userId) REFERENCES users(userId))')
+conn.execute('CREATE TABLE IF NOT EXISTS mapPointers (pointerId Integer PRIMARY KEY, long REAL NOT NULL, lan REAL NOT NULL, description TEXT, likes INTEGER, createdOn TEXT NOT NULL, userId INTEGER NOT NULL, FOREIGN KEY(userId) REFERENCES users(userId))')
 cur = conn.cursor()
 cur.execute("INSERT OR IGNORE INTO users (email,username,password,isAdmin) VALUES (?,?,?,?)",('test@email.com','Admin','123', True) )
 conn.commit()
